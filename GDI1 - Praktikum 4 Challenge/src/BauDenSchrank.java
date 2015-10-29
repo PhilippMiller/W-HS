db9a7e91e0757fd24ac735c233ed89fecf32510f
PhilippMiller <philipp.miller@gmx.de>
2015-10-29 10:34:49 +0100

    Create project and import BasicIO.java and BauDenSchrank.java

---
+++ GDI1 - Praktikum 4 Challenge/src/BauDenSchrank.java
@@ -0,0 +1,65 @@
+
+public class BauDenSchrank
+{
+	
+	public void buildIt(int[] Schubladen)
+	{
+		
+		String Inhalt;
+		int InhaltGesamt = 0;
+		int InhaltGesamt_add = 0;
+		
+		
+		System.out.println("\nDein Schrank");
+		
+		for(int i = 0; Schubladen.length > i; i++)
+		{
+			Inhalt = "" + Schubladen[i];
+			InhaltGesamt = InhaltGesamt + Inhalt.length();
+			
+		}
+		
+		InhaltGesamt = InhaltGesamt + Schubladen.length*2 - 2;
+		
+		System.out.print("o");
+		while( InhaltGesamt > InhaltGesamt_add)
+		{
+			System.out.print("_");
+			InhaltGesamt_add++;
+		}
+		System.out.print("o");
+		
+		System.out.println("");
+		
+		for(int i = 0; Schubladen.length > i; i++)
+		{
+			System.out.print("["+Schubladen[i]+"]");
+			
+			
+		}
+		System.out.println("");
+		
+		InhaltGesamt_add = 0;
+		System.out.print("|");
+		while( InhaltGesamt > InhaltGesamt_add)
+		{
+			System.out.print("_");
+			InhaltGesamt_add++;
+		}
+		System.out.print("|");
+		System.out.println("");
+		
+		InhaltGesamt_add = 0;
+		System.out.print("o");
+		while( InhaltGesamt > InhaltGesamt_add)
+		{
+			System.out.print(" ");
+			InhaltGesamt_add++;
+		}
+		System.out.print("o");
+		System.out.println("");
+		
+		System.out.println("\n");
+	}
+	
+}
