ce34d3c644f8ce196b1f334b4fed4cdce3125d7d
PhilippMiller <philipp.miller@gmx.de>
2015-11-05 22:36:48 +0100

    sort it

--- JKbF - DotComVersenkenV1/src/Main.java
+++ JKbF - DotComVersenkenV1/src/Main.java
@@ -1,77 +1,66 @@
 
-public class Main
-{
+public class Main {
 	int[] zellorte;
-	int anzahlTreffer = 0;	
+	int anzahlTreffer = 0;
 
-	//VOID MAIN
-	public static void main(String[] args)
-	{
+	// VOID MAIN
+	public static void main(String[] args) {
 		int anzahlTipps = 0;
 		SpielHelfer helfer = new SpielHelfer();
-		
+
 		Main dotCom = new Main();
-		
+
 		int zufallsZahl = (int) (Math.random() * 5);
-		
-		int[] orte = {zufallsZahl,zufallsZahl+1,zufallsZahl+2};
+
+		int[] orte = { zufallsZahl, zufallsZahl + 1, zufallsZahl + 2 };
 		dotCom.setZellorte(orte);
-		
+
 		boolean lebt = true;
-		
-		while (lebt == true)
-		{
-			
+
+		while (lebt == true) {
+
 			String tipp = helfer.getBenutzereingabe("Geben Sie eine Zahl ein.");
 			String ergebnis = dotCom.prüfDich(tipp);
 			anzahlTipps++;
-			
-			if (ergebnis.equals("Versenkt"))
-			{
+
+			if (ergebnis.equals("Versenkt")) {
 				lebt = false;
 				System.out.println("Sie haben " + anzahlTipps + " Versuche benöigt!");
 			}
-		
+
 		}
-				
+
 	}
-	
-	
+
 	// prüfDich()
-	public String prüfDich(String stringTipp)
-	{
-		
+	public String prüfDich(String stringTipp) {
+
 		int tipp = Integer.parseInt(stringTipp);
 		String ergebnis = "Vorbei";
-		
-		for (int zelle : zellorte)
-		{
-			if (tipp == zelle)
-			{
+
+		for (int zelle : zellorte) {
+			if (tipp == zelle) {
 				ergebnis = "Treffer";
 				anzahlTreffer++;
-				
+
 				break;
 			}
 		}
-		
-		if (anzahlTreffer == zellorte.length)
-		{
+
+		if (anzahlTreffer == zellorte.length) {
 			ergebnis = "Versenkt";
 		}
-		
+
 		System.out.println(ergebnis);
 		return ergebnis;
-		
+
 	}
-	
-	
+
 	// setZellorte()
-	void setZellorte(int[] orte)
-	{
-		
+	void setZellorte(int[] orte) {
+
 		zellorte = orte;
-		
+
 	}
 
 }
