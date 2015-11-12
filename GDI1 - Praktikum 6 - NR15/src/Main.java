c24eb5f486a80613020342e50d8ea1274d6f6fbe
PhilippMiller <philipp.miller@gmx.de>
2015-11-12 13:26:31 +0100

    add 6 times (for...)

--- GDI1 - Praktikum 6 - NR15/src/Main.java
+++ GDI1 - Praktikum 6 - NR15/src/Main.java
@@ -6,23 +6,26 @@ public class Main {
 	static Rennauto VW = new Rennauto(3, "Marie", "VW", "grün", 194);
 
 	public static void main(String[] args) {
+
 		fahren();
 	}
 
 	static void fahren() {
-		System.out.println("Trainingssession 2:");
-		System.out.println("===================");
+		for (int i = 0; i < 6; i++) {
+			System.out.println("Trainingssession "+(i+1)+":");
+			System.out.println("===================");
 
-		BMW.fahren();
-		OPEL.fahren();
-		VW.fahren();
+			BMW.fahren();
+			OPEL.fahren();
+			VW.fahren();
 
-		System.out.println("Der " + BMW.getFarbe() + " " + BMW.getName() + ", gefahren von " + BMW.getFahrer()
-				+ ", hat jetzt " + BMW.getRundenzahl() + " Runden zurückgelegt.");
-		System.out.println("Der " + OPEL.getFarbe() + " " + OPEL.getName() + ", gefahren von " + OPEL.getFahrer()
-				+ ", hat jetzt " + OPEL.getRundenzahl() + " Runden zurückgelegt.");
-		System.out.println("Der " + VW.getFarbe() + " " + VW.getName() + ", gefahren von " + VW.getFahrer()
-				+ ", hat jetzt " + VW.getRundenzahl() + " Runden zurückgelegt.");
+			System.out.println("Der " + BMW.getFarbe() + " " + BMW.getName() + ", gefahren von " + BMW.getFahrer()
+					+ ", hat jetzt " + BMW.getRundenzahl() + " Runden zurückgelegt.");
+			System.out.println("Der " + OPEL.getFarbe() + " " + OPEL.getName() + ", gefahren von " + OPEL.getFahrer()
+					+ ", hat jetzt " + OPEL.getRundenzahl() + " Runden zurückgelegt.");
+			System.out.println("Der " + VW.getFarbe() + " " + VW.getName() + ", gefahren von " + VW.getFahrer()
+					+ ", hat jetzt " + VW.getRundenzahl() + " Runden zurückgelegt.");
+		}
 	}
 
 }
