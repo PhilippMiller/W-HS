22246221b383961760a19274b2aed06e3fb78dda
PhilippMiller <philipp.miller@gmx.de>
2015-11-12 13:18:10 +0100

    finish NR15

--- GDI1 - Praktikum 6 - NR15/src/Main.java
+++ GDI1 - Praktikum 6 - NR15/src/Main.java
@@ -1,19 +1,28 @@
 
 public class Main {
 
+	static Rennauto BMW = new Rennauto(1, "Klaus", "BMW", "blau", 200);
+	static Rennauto OPEL = new Rennauto(2, "Peter", "OPEL", "rot", 190);
+	static Rennauto VW = new Rennauto(3, "Marie", "VW", "grün", 194);
+
 	public static void main(String[] args) {
-		
-		Rennauto BMW = new Rennauto(1, "Klaus", "BMW", "blau", 200);
-		Rennauto OPEL = new Rennauto(2, "Peter", "OPEL", "rot", 190);
-		Rennauto VW = new Rennauto(3, "Marie", "VW", "grün", 194); 
-		
-		System.out.println("Der Fahrer des " + BMW.getFarbe() + " BMW heißt Klaus!");
-		System.out.println("Die Höchstgeschwindigkeit des " + OPEL.getFarbe() + " " + OPEL.getName() + " liegt bei " + OPEL.getHöchstgeschwindigkeit() + "km/h!");
-		System.out.println("Der " + VW.getFarbe() + " " + VW.getName() + " hat die Startnummer " + VW.getAutonummer() + "."); 
-		
-		
+		fahren();
+	}
 
+	static void fahren() {
+		System.out.println("Trainingssession 2:");
+		System.out.println("===================");
 
+		BMW.fahren();
+		OPEL.fahren();
+		VW.fahren();
+
+		System.out.println("Der " + BMW.getFarbe() + " " + BMW.getName() + ", gefahren von " + BMW.getFahrer()
+				+ ", hat jetzt " + BMW.getRundenzahl() + " Runden zurückgelegt.");
+		System.out.println("Der " + OPEL.getFarbe() + " " + OPEL.getName() + ", gefahren von " + OPEL.getFahrer()
+				+ ", hat jetzt " + OPEL.getRundenzahl() + " Runden zurückgelegt.");
+		System.out.println("Der " + VW.getFarbe() + " " + VW.getName() + ", gefahren von " + VW.getFahrer()
+				+ ", hat jetzt " + VW.getRundenzahl() + " Runden zurückgelegt.");
 	}
 
 }
