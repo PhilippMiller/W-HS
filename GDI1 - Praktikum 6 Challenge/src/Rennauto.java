3ce69959ce827ac8e9055107ea7c8157d8765818
PhilippMiller <philipp.miller@gmx.de>
2015-11-12 13:20:43 +0100

    create project import Main.java import Rennauto.java

---
+++ GDI1 - Praktikum 6 Challenge/src/Rennauto.java
@@ -0,0 +1,73 @@
+
+public class Rennauto {
+
+	int autonummer;
+	String fahrer;
+	String name;
+	String farbe;
+	int höchstgeschwindigkeit;
+	int rundenzahl;
+
+	public Rennauto(int autonummer, String fahrer, String name, String farbe, int höchstgeschwindigkeit) {
+		setAutonummer(autonummer);
+		setFahrer(fahrer);
+		setName(name);
+		setFarbe(farbe);
+		setHöchstgeschwindigkeit(höchstgeschwindigkeit);
+	}
+
+	public int getAutonummer() {
+		return autonummer;
+	}
+
+	public void setAutonummer(int autonummer) {
+		this.autonummer = autonummer;
+	}
+
+	public String getFahrer() {
+		return fahrer;
+	}
+
+	public void setFahrer(String fahrer) {
+		this.fahrer = fahrer;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFarbe() {
+		return farbe;
+	}
+
+	public void setFarbe(String farbe) {
+		this.farbe = farbe;
+	}
+
+	public int getHöchstgeschwindigkeit() {
+		return höchstgeschwindigkeit;
+	}
+
+	public void setHöchstgeschwindigkeit(int höchstgeschwindigkeit) {
+		this.höchstgeschwindigkeit = höchstgeschwindigkeit;
+	}
+
+	public int getRundenzahl() {
+		return rundenzahl;
+	}
+
+	public void setRundenzahl(int rundenzahl) {
+		this.rundenzahl = rundenzahl;
+	}
+
+	public void fahren() {
+		double faktor = Math.random() * 0.25 + 0.25;
+		double berechnung = this.höchstgeschwindigkeit * faktor;
+		this.rundenzahl = this.rundenzahl + (int) berechnung;
+	}
+
+}
