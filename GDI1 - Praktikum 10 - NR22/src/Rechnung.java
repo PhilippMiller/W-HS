72eb41b47c28f39e7b2cd07ee450e7ab46fa6daf
PhilippMiller <philipp.miller@gmx.de>
2016-01-07 13:00:21 +0100

    fix

--- GDI1 - Praktikum 10 - NR22/src/Rechnung.java
+++ GDI1 - Praktikum 10 - NR22/src/Rechnung.java
@@ -1,13 +1,15 @@
 
 public class Rechnung {
-	
+
 	private static int rechnungsnummer = 10000;
-	
-	public static void RechnungErstellen(Stammkunde kunde){
-		String rechnung = "########################\n\n" + kunde.getVorname() + " " + kunde.getName() + "\n\nRechnungsnummer:\n" + rechnungsnummer + "\nRechnungssumme:\n" + kunde.getRechnungsWert() + "\n\n########################";
+
+	public static void RechnungErstellen(Stammkunde kunde) {
+		kunde.setRechnungsnummer(rechnungsnummer);
+		String rechnung = "########################\n\n" + kunde.getVorname() + " " + kunde.getName()
+				+ "\n\nRechnungsnummer:\n" + rechnungsnummer + "\nRechnungssumme:\n" + kunde.getRechnungsWert()
+				+ "\n\n########################";
 		System.out.println(rechnung);
 		rechnungsnummer++;
 	}
-	
 
 }
--- GDI1 - Praktikum 10 - NR22/src/Stammkunde.java
+++ GDI1 - Praktikum 10 - NR22/src/Stammkunde.java
@@ -6,7 +6,9 @@ public class Stammkunde {
 	private double rechnungsWert;
 	private int kundennummer;
 	private static int anzahlKunden;
-	
+	private int rechnungsnummer;
+
+
 	public Stammkunde(String name, String vorname) {
 		setName(name);
 		setVorname(vorname);
@@ -53,6 +55,16 @@ public class Stammkunde {
 
 	public void setRechnungsWert(double rechnungsWert) {
 		this.rechnungsWert = rechnungsWert;
+	}
+	
+	public int getRechnungsnummer() {
+		return rechnungsnummer;
+	}
+
+
+
+	public void setRechnungsnummer(int rechnungsnummer) {
+		this.rechnungsnummer = rechnungsnummer;
 	}
 
 }
