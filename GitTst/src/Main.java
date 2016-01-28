f70cfda94706b0fca87b149427ce578724e3ec48
PhilippMiller <philipp.miller@gmx.de>
2016-01-28 18:59:53 +0100

    some simple testing code

--- GitTst/src/Main.java
+++ GitTst/src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
-	public static void main(String std){
-		System.out.print("Hello world");
+	public static void main(String args[]){
+		System.out.print("Hello world\n\n");
+		
+		int[][] f = {{1,2},{3,4},{5,6}};
+		System.out.println(f[0][f[0][0]]);
 	}
 }
