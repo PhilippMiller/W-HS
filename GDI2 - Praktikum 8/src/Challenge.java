745bbe4822006b8db9c346f82b53f843d7a5b39e
PhilippMiller <philipp.miller@gmx.de>
2016-06-22 00:35:50 +0200

    removed unused import (FileNotFoundException)

--- GDI2 - Praktikum 8/src/Challenge.java
+++ GDI2 - Praktikum 8/src/Challenge.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
